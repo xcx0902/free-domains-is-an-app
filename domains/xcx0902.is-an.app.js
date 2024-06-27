f0d75e7296331999e2c58ff65f4876e1b6aa4db4
Xu Chenxuan <99001676+xcx0902@users.noreply.github.com>
2024-06-27 14:56:46 +0800

    Create xcx0902.is-an.app.js

---
+++ domains/xcx0902.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Personal website',
+  domain: 'is-an.app',
+  subdomain: 'xcx0902',
+  owner: {
+    repo: 'https://github.com/xcx0902/BetaOJ',
+    email: '3562907147@qq.com',
+  },
+  record: {
+    CNAME: 'cn-hk-bgp-4.of-7af93c01.shop',
+  },
+  proxy: false,
+})
